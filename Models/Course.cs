c51a4dbd0d9c7789f35ae41b24e4ad3b36709f4c
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2023-12-03 22:04:16 +0000

    Add Course.cs Model

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieSphere.Models;
 
 public class Course
 {
+    [Key]
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Tag { get; set; }
+    
+    //NavigationProperties
+    public ICollection<ApplicationUser> UsersInCourse { get; set; }
 }
