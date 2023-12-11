2c337015281733a2d6134b5c93a6a7cf35ab0c72
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2023-12-11 17:10:08 +0000

    ¯\_(ツ)_/¯

--- obj/Debug/net8.0/MovieSphere.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieSphere.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5567a636ef71176dd2ad8d219b7924c8e57d90d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe7d046342ab0a1866380e474d0667f0e2703c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
