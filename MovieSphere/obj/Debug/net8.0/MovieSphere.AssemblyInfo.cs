722c9e2d0871a80ec6797c09265732224ea3a36f
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2023-12-20 20:25:49 +0000

    Onwards to -> 4:46:23

--- MovieSphere/obj/Debug/net8.0/MovieSphere.AssemblyInfo.cs
+++ MovieSphere/obj/Debug/net8.0/MovieSphere.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef6754c7c6d446fbcf18d1e2fd48c53fe73a69d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6cbe2996318233dfd6fbcf1b7fc3fd319ae1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Movie Sphere
 Final Project for Web Programming Class
 
-reference: https://www.youtube.com/watch?v=AopeJjkcRvU
+Video Reference: https://www.youtube.com/watch?v=AopeJjkcRvU
+
+Notes:
+- if migratinos dont work, on the package manager console, check on what project it is running
